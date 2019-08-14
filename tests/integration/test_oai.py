d2a7770ad4f264c8c8effea3c440bd27851db055
Richard Hallett <rhallett@datacite.org>
2019-08-14 15:53:00 +0200

    Add intgeration test for listRecord with dc

--- tests/integration/test_oai.py
+++ tests/integration/test_oai.py
@@ -1,5 +1,6 @@
 """Tests for http endpoints of OAI-PMH verbs"""
 
+import datetime
 from lxml import etree
 
 from . import factories
@@ -39,6 +40,7 @@ def test_identify(client):
 
 def test_get_record_dc(client, mocker):
     """Test the getRecord verb responds and conforms as expected"""
+
     # Mock the datacite service to ensure the same record data is returned.
     mocked_get_metadata = mocker.patch('viringo.services.datacite.get_metadata')
     # Get fake result
@@ -56,6 +58,46 @@ def test_get_record_dc(client, mocker):
         'tests/integration/fixtures/oai_getrecord_dc.xml',
         response.get_data(),
         "GetRecord"
+    )
+
+    # Compare the main part of the request against test case
+    assert original == target
+
+def test_list_records_dc(client, mocker):
+    """Test the listRecords verb responds and conforms as expected"""
+
+    # Mock the datacite service to ensure the same record data is returned.
+    mocked_get_metadata_list = mocker.patch('viringo.services.datacite.get_metadata_list')
+    # Get fake results
+    result_1 = factories.MetadataFactory()
+    result_2 = factories.MetadataFactory(
+        identifier="10.5072/not-a-real-doi-2",
+        created_datetime=datetime.datetime(2018, 5, 17, 6, 33),
+        dates=[
+            {'type': 'Issued', 'date': '2018-02-16'},
+            {'type': 'Created', 'date': '2018-02-16'},
+            {'type': 'Updated', 'date': '2018-02-16'}
+        ],
+        identifiers=[
+            {'type': 'DOI', 'identifier': '10.5072/not-a-real-doi-2'}
+        ],
+        relations=[]
+    )
+    results = [result_1, result_2], 1
+
+    # Set the mocked service to use the fake result
+    mocked_get_metadata_list.return_value = results
+
+    response = client.get('/?verb=ListRecords&metadataPrefix=oai_dc')
+
+    assert response.status_code == 200
+    assert response.content_type == 'application/xml; charset=utf-8'
+
+    # Compare just the verb part of the oai xml
+    original, target = construct_oai_xml_comparisons(
+        'tests/integration/fixtures/oai_listrecords_dc.xml',
+        response.get_data(),
+        "ListRecords"
     )
 
     # Compare the main part of the request against test case
