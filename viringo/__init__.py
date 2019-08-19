4a661f4800ba74cc9c12dba3ff2dd9a143bd676d
Richard Hallett <rhallett@datacite.org>
2019-08-19 18:22:03 +0200

    Remove secret key as we don't need sessions

--- viringo/__init__.py
+++ viringo/__init__.py
@@ -10,10 +10,7 @@ class DefaultResponse(Response):
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        # a default secret that should be overridden by instance config
-        SECRET_KEY='dev',
-    )
+    app.config.from_mapping()
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
