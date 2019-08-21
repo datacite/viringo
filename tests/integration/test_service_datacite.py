fbda295b86a86280332fdb5efaa88e3b0fcd67b2
Richard Hallett <rhallett@datacite.org>
2019-08-21 12:01:41 +0200

    Add test for datacite service getMetadata

--- tests/integration/test_service_datacite.py
+++ tests/integration/test_service_datacite.py
@@ -4,8 +4,42 @@ import json
 from viringo.services import datacite
 
 # Mock the requests json return
-def test_get_metadata():
-    pass
+def test_get_metadata(mocker):
+    """Tests the results of the datasite service for getting a single metadata record"""
+    # Mock the datacite service to ensure the same record data is returned.
+    mocked_requests_get = mocker.patch('viringo.services.datacite.requests.get')
+
+    with open('tests/integration/fixtures/datacite_api_doi.json') as json_file:
+        data = json.load(json_file)
+
+    # Set the mocked service to use the fake result
+    mocked_requests_get.return_value.status_code = 200
+    mocked_requests_get.return_value.json.return_value = data
+
+    metadata = datacite.get_metadata("10.5438/prvv-nv23")
+
+    # Compare some key elements to see if it's what we expect
+    assert metadata.identifier == "10.5438/prvv-nv23"
+    assert metadata.titles == ['Welcome to the DataCite Team, Richard!']
+    assert metadata.creators == ['Fenner, Martin', 'Hallett, Richard']
+    assert metadata.descriptions == ['DataCite is pleased to welcome Richard \
+Hallett to our team. Richard joined DataCite as application developer \
+in December. Get to know him better via this interview. Richard Hallett \
+Can you tell us a little bit about what you did before you started...']
+    assert metadata.subjects == ['datacite']
+    assert metadata.publisher == 'DataCite'
+    assert metadata.publication_year == 2018
+    assert metadata.dates == [
+        {'type': 'Issued', 'date': '2018-01-18'},
+        {'type': 'Created', 'date': '2018-01-18'},
+        {'type': 'Updated', 'date': '2018-01-18'}
+    ]
+    assert metadata.resource_types == ['Text']
+    assert metadata.identifiers == [{'type': 'DOI', 'identifier': '10.5438/prvv-nv23'}]
+    assert metadata.relations == [{'type': 'DOI', 'identifier': '10.5438/0000-00ss'}]
+    assert metadata.client == 'DATACITE.BLOG'
+    assert metadata.active
+
 
 # Mock the requests json return
 def test_get_metadata_list():
