599ec35bd6bcdef6a1ed60e752127df62522eaf0
Martin Fenner <martin.fenner@datacite.org>
2019-11-09 08:28:29 +0100

    add dotenv support. #32

--- viringo/wsgi.py
+++ viringo/wsgi.py
@@ -1,18 +1,25 @@
 """Script for running WSGI process"""
 
+import os
 import sys
 import logging
 import json_log_formatter
+from dotenv import load_dotenv
 
 from viringo import create_app
 
+load_dotenv()
+
 application = create_app()
 
+# Configure logging
+stdout_handler = logging.StreamHandler(sys.stdout)
+
+# Use JSON stdout handler if not in debug mode
 if not application.debug:
     formatter = json_log_formatter.JSONFormatter()
-    json_handler = logging.StreamHandler(sys.stdout)
-    json_handler.setFormatter(formatter)
+    stdout_handler.setFormatter(formatter)
 
-    # Set the only handler to be our JSON stdout handler
-    logger = logging.getLogger()
-    logger.handlers = [json_handler]
+logger = logging.getLogger('werkzeug')
+logger.setLevel(os.getenv('LOG_LEVEL', 'INFO'))
+logger.handlers = [stdout_handler]
